82704ad3ab7c9cba557b219d43b03197648d761c
Mikael Jagekrans <mikael.jagekrans@boardintelligence.co.uk>
2016-07-05 19:12:19 +0100

    getting closer

--- bin/hls_segment.py
+++ bin/hls_segment.py
@@ -7,7 +7,7 @@ def transmux(source_path, playlist_type, bit_rate, segno):
     stream_index = get_stream_index(source_path, playlist_type, bit_rate)['index']
     output = os.path.join('static', playlist_type, bit_rate, '{0}.ts'.format(segno))
 
-    cmd = 'ffmpeg -y -ss {0} -t {1} -i {2} -map 0:{3} -f mpegts -mpegts_copyts 1 {4}'
+    cmd = 'ffmpeg -y -ss {0} -t {1} -i {2} -map 0:{3} -c:v libx264 -bsf:v h264_mp4toannexb -f mpegts {4}'
     cmd = cmd.format(SEGMENT_LENGTH * segno, SEGMENT_LENGTH, source_path, stream_index, output)
     call(cmd.split())
 
